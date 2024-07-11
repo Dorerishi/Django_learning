e363d233d24e6fd0452620c577660a8264f56540
Dorerishi <hrushikeshraghvendra@gmail.com>
2024-07-11 10:56:47 +0530

    till / before pagination

--- Django/django_project/users/admin.py
+++ Django/django_project/users/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile
 
-from .models import Post
-
-admin.site.register(Post)
-
+admin.site.register(Profile)
